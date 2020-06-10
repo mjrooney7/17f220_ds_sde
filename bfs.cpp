8b96fc657dd8d65b95e163c4437b676eb8ca6b46
Manojkumar <48024548+mjrooney7@users.noreply.github.com>
2020-06-10 23:31:28 +0530

    Add files via upload
    initial commit

--- bfs.cpp
+++ bfs.cpp
@@ -2,25 +2,35 @@
 using namespace std;
 vector<long long int>v[51];
 long long int visited[51]={0};
-void dfs(long long int ver)
+void bfs(long long int ver)
 {
-	long long int j;
-	cout<<ver<<" -> ";
+	long long int j,e;
+	queue<long long int>q;
 	visited[ver]=1;
-	for(j=0;j<v[ver].size();j++)
+	q.push(ver);
+	cout<<ver<<" -> ";
+	while(!q.empty())
 	{
-		if(visited[v[ver][j]]==0)
+		e=q.front();
+		q.pop();
+		for(j=0;j<v[e].size();j++)
 		{
-			dfs(v[ver][j]);
+			if(visited[v[e][j]]==0)
+			{
+				q.push(v[e][j]);
+				cout<<v[e][j]<<" -> ";
+				visited[v[e][j]]=1;
+			}
 		}
 	}
+	
 }
 int main()
 {
 	long long int n,i,j,e,st,en;
 	cout<<"Enter no of nodes:";
 	cin>>n;
-	cout<<"Enter no of vertices:";
+	cout<<"Enter no of edges:";
 	cin>>e;
 	while(e--)
 	{
@@ -30,7 +40,7 @@ int main()
 	}
 	cout<<"Enter starting node:";
 	cin>>st;
-	dfs(st);
+	bfs(st);
 	cout<<" end "<<endl;
 	return 0;
 	
